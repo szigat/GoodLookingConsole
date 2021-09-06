286f9c12579284558e9fb61053aeb8e464e9267d
Attila Szigeti <szigat@gmail.com>
2021-09-07 00:18:12 +0200

    prompt

--- Program.cs
+++ Program.cs
@@ -15,6 +15,31 @@ namespace GoodLookingConsole
                 new FigletText("Hello world")
                 .LeftAligned()
                 .Color(Color.Red));
+
+            var selectedItem = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title<string>("What's your [green]favorite fruit[/]?")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                    .AddChoices<string>(new[] {
+                        "", "Avocado", 
+                        "Banana", "Blackcurrant", "Blueberry",
+                        "Cherry", "Cloudberry", "Cocunut",
+                    })
+                    );
+
+            var table = new Table();
+
+            table.AddColumn(new TableColumn("Id").Centered());
+            table.AddColumn(new TableColumn("Name").Centered());
+            table.AddColumn(new TableColumn("Status").Centered());
+
+
+            table.AddRow("1", "Alpha", "Enabled");
+            table.AddRow("2", "Beta", "Enabled");
+            table.AddRow("3", "Gamma", "Enabled");
+
+            AnsiConsole.Render(table);
         }
     }
 }
