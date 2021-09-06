4d42a8127d9f2c3c50034922a0e9d022e2bab9e2
Attila Szigeti <szigat@gmail.com>
2021-09-06 21:49:59 +0200

    hello world

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Spectre.Console;
+
+namespace GoodLookingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.Markup("[underline red]Hello[/] World!");
+
+            Console.WriteLine();
+
+            AnsiConsole.Render(
+                new FigletText("Hello world")
+                .LeftAligned()
+                .Color(Color.Red));
+        }
+    }
+}
